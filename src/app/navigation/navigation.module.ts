70f0430ada87b1399f6ebe18446280c8e47208bc
Daniel Cavalcante Scalisse <daniel_scalisse@yahoo.com.br>
2025-09-16 19:17:37 -0300

    Update navigation.module.ts
    Ordenação das referências dos componentes no módulo de navegação.

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -2,23 +2,23 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { AccessDeniedComponent } from './access-denied/access-denied.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { MenuLoginComponent } from './menu-login/menu-login.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AccessDeniedComponent } from './access-denied/access-denied.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [
-        AccessDeniedComponent,
-        FooterComponent,
-        HomeComponent,
         MenuComponent,
         MenuLoginComponent,
-        NotFoundComponent
+        HomeComponent,
+        FooterComponent,
+        NotFoundComponent,
+        AccessDeniedComponent
     ],
     imports: [
         CommonModule,
@@ -26,12 +26,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
         NgbModule
     ],
     exports: [
-        AccessDeniedComponent,
-        FooterComponent,
-        HomeComponent,
         MenuComponent,
         MenuLoginComponent,
+        HomeComponent,
+        FooterComponent,
         NotFoundComponent
+        AccessDeniedComponent,
     ],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
