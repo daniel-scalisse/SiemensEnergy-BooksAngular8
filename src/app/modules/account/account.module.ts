073bd0f912a44bcd566354d41efc2b1acb163d71
Daniel Cavalcante Scalisse <daniel_scalisse@yahoo.com.br>
2025-12-03 22:39:49 -0300

    Ordenação das referências conforme o uso.

--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { NgxSpinnerModule } from 'ngx-spinner';
 
+import { AccountRoutingModule } from './account.route';
 import { AccountAppComponent } from './account.app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 
-import { AccountRoutingModule } from './account.route';
-
 import { AccountService } from './services/account.service';
 import { AccountGuard } from './services/account.guard';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
